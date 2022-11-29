239d33c87aa5681c5ced6fa040ace83065ee3ba0
Gergely Koloszar <gergely.koloszar@gmail.com>
2022-11-29 11:45:49 +0100

    Add PUT method to solar panels, and pwr manager
    Full interface is now implemented

--- inc/server.h
+++ inc/server.h
@@ -23,14 +23,9 @@ class TstResource : public httpserver::http_resource {
 private:
   json msg;
 public:
-  TstResource(){
-    msg = json::parse(R"({ "address" : "0x07", "data" : "0x55" })");
-  }
+  TstResource();
 
-  std::shared_ptr<http_response> render_POST(const http_request& param) {
-    const std::string data = param.get_content();
-    return std::shared_ptr<http_response>(new string_response(data.c_str()));
-  }
+  std::shared_ptr<http_response> render_POST(const http_request& param);
 };
 
 /** @class Probestatusresource
@@ -54,90 +49,10 @@ private:
 public:
   ProbeBatteryResource(Probe* p_m_arg) : sim_model(p_m_arg) {}
 
-  std::shared_ptr<http_response> render_PUT(const http_request& request) {
-
-    std::string response = "Ok";
-    int response_code = 200;
-    
-    json msg = json::parse(request.get_content());
-    std::string name = msg["name"];
-    std::string command = msg["command"];
-
-    if(name.empty() || command.empty()){
-      response = "Invalid JSON";
-      response_code = 400;
-      return std::shared_ptr<http_response>(new string_response(response, response_code));
-    }
-
-    bool exist = false;
-    if(command == "charging"){
-      if(!sim_model->power_manager().set_battery_charging(name, msg["value"])){
-	response = "No battery with this name present";
-	response_code = 400;
-      }
-    } else if(command == "enabled"){
-      if(!sim_model->power_manager().enable_battery(name)){
-	response = "No battery with this name present";
-	response_code = 400;
-      }
-    } else if(command == "rename"){
-      if(!sim_model->power_manager().rename_battery(name, msg["value"])){
-	response = "No battery with this name present";
-	response_code = 400;
-      }
-    } else {
-      response = "Invalid command";
-      response_code = 400;
-    }
-    
-    return std::shared_ptr<http_response>(new string_response(response, response_code));
-  }
-
-  std::shared_ptr<http_response> render_POST(const http_request& request) {
-    std::string response = "Ok";
-    int response_code = 200;
-
-    json msg = json::parse(request.get_content());
-    std::string bat_name = msg["name"];
-    uint32_t charge_level = msg["charge_level"];
-
-    if(bat_name.empty() || !msg.contains("charge_level")){
-      response = "Invalid JSON";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    sim_model->power_manager().add_battery(Battery{
-	charge_level,
-	bat_name
-      });
-
-    return std::shared_ptr<http_response> (new string_response(response, response_code));
-  }
-
-  std::shared_ptr<http_response> render_DELETE(const http_request& request) {
-    std::string response = "Ok";
-    int response_code = 200;
-
-    json msg = json::parse(request.get_content());
-
-    if(!msg.contains("name")){
-      response = "Invalid JSON";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    std::string name = msg["name"];
-
-    bool success = sim_model->power_manager().remove_battery(name);
-    if(!success){
-      response = "No such battery";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    return std::shared_ptr<http_response> (new string_response(response, response_code));
-  }
+  std::shared_ptr<http_response> render_PUT(const http_request& request);
+  std::shared_ptr<http_response> render_POST(const http_request& request);
+  std::shared_ptr<http_response> render_DELETE(const http_request& request);
+  
 };
 
 class ProbeSolarPanelResource : public httpserver::http_resource {
@@ -146,59 +61,9 @@ private:
 public:
   ProbeSolarPanelResource(Probe* p_m_arg) : sim_model(p_m_arg) {}
 
-  std::shared_ptr<http_response> render_POST(const http_request& request) { 
-    std::string response = "Ok";
-    int response_code = 200;
-
-    json msg = json::parse(request.get_content());
-    std::string sol_name = msg["name"];
-    uint8_t extract_level = msg["extract_level"];
-
-    if(sol_name.empty() || !msg.contains("extract_level")){
-      response = "Invalid JSON";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    sim_model->power_manager().add_solar_panel(SolarPanel{
-	sol_name,
-	extract_level
-      });
-
-    return std::shared_ptr<http_response> (new string_response(response, response_code));
-  }
-
-  std::shared_ptr<http_response> render_PUT(const http_request& request) {
-
-    /*TODO*/
-
-    
-  }
-
-  std::shared_ptr<http_response> render_DELETE(const http_request& request) {
-    
-    std::string response = "Ok";
-    int response_code = 200;
-
-    json msg = json::parse(request.get_content());
-
-    if(!msg.contains("name")){
-      response = "Invalid JSON";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    std::string name = msg["name"];
-
-    bool success = sim_model->power_manager().remove_solar_panel(name);
-    if(!success){
-      response = "No such battery";
-      response_code = 400;
-      return std::shared_ptr<http_response> (new string_response(response, response_code));
-    }
-
-    return std::shared_ptr<http_response> (new string_response(response, response_code));
-  }
+  std::shared_ptr<http_response> render_POST(const http_request& request);
+  std::shared_ptr<http_response> render_PUT(const http_request& request);
+  std::shared_ptr<http_response> render_DELETE(const http_request& request);
 
 };
 
@@ -208,10 +73,17 @@ private:
 public:
   ProbeStopResource(Probe* p_m_arg) : sim_model(p_m_arg) {}
 
-  std::shared_ptr<http_response> render_POST(const http_request& param) {
-    sim_model->stop_simulation();
-    return std::shared_ptr<http_response>(new string_response("Shutting down!"));
-  }
+  std::shared_ptr<http_response> render_POST(const http_request &param);
+};
+
+class ProbePwrManagerResource : public httpserver::http_resource {
+private:
+  Probe* sim_model;
+public:
+  ProbePwrManagerResource(Probe* p_m_arg) : sim_model(p_m_arg) {}
+
+  std::shared_ptr<http_response> render_PUT(const http_request &param);
+  
 };
 
 /**@struct thread_args
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <microhttpd.h>
 #include <semaphore.h>
 
 #include "server.h"
@@ -34,4 +35,209 @@ void* http_server_thread(void * thread_arg){
   ws->start(true);
 
   pthread_exit(NULL);
+}
+
+
+
+
+
+TstResource::TstResource() {
+  msg = json::parse(R"({ "address" : "0x07", "data" : "0x55" })");
+}
+
+std::shared_ptr<http_response> TstResource::render_POST(const http_request& param) {
+  const std::string data = param.get_content();
+  return std::shared_ptr<http_response>(new string_response(data.c_str()));
+}
+
+
+std::shared_ptr<http_response> ProbeBatteryResource::render_PUT(const http_request &request) {
+
+  std::string response = "Ok";
+  int response_code = 200;
+    
+  json msg = json::parse(request.get_content());
+  std::string name = msg["name"];
+  std::string command = msg["command"];
+  
+  if(name.empty() || command.empty()){
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response>(new string_response(response, response_code));
+  }
+  
+  if(command == "charging"){
+    if(!sim_model->power_manager().set_battery_charging(name, msg["value"])){
+      response = "No battery with this name present";
+      response_code = 400;
+    }
+  } else if(command == "enabled"){
+    if(!sim_model->power_manager().enable_battery(name)){
+      response = "No battery with this name present";
+      response_code = 400;
+    }
+  } else if(command == "rename"){
+    if(!sim_model->power_manager().rename_battery(name, msg["value"])){
+      response = "No battery with this name present";
+      response_code = 400;
+    }
+  } else {
+    response = "Invalid command";
+    response_code = 400;
+  }
+  
+  return std::shared_ptr<http_response>(new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response> ProbeBatteryResource::render_POST(const http_request& request) {
+  std::string response = "Ok";
+  int response_code = 200;
+  
+  json msg = json::parse(request.get_content());
+  std::string bat_name = msg["name"];
+  uint32_t charge_level = msg["charge_level"];
+  
+  if(bat_name.empty() || !msg.contains("charge_level")){
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response> (new string_response(response, response_code));
+  }
+  
+  sim_model->power_manager().add_battery(Battery{
+      charge_level,
+      bat_name
+    });
+  
+  return std::shared_ptr<http_response> (new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response> ProbeBatteryResource::render_DELETE(const http_request& request) {
+  std::string response = "Ok";
+  int response_code = 200;
+  
+  json msg = json::parse(request.get_content());
+  
+  if(!msg.contains("name")){
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response> (new string_response(response, response_code));
+  }
+  
+  std::string name = msg["name"];
+  
+  bool success = sim_model->power_manager().remove_battery(name);
+  if(!success){
+    response = "No such battery";
+    response_code = 400;
+    return std::shared_ptr<http_response> (new string_response(response, response_code));
+  }
+  
+  return std::shared_ptr<http_response> (new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response> ProbeSolarPanelResource::render_POST(const http_request& request) { 
+  std::string response = "Ok";
+  int response_code = 200;
+  
+  json msg = json::parse(request.get_content());
+  std::string sol_name = msg["name"];
+  uint8_t extract_level = msg["extract_level"];
+  
+  if(sol_name.empty() || !msg.contains("extract_level")){
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response> (new string_response(response, response_code));
+  }
+  
+  sim_model->power_manager().add_solar_panel(SolarPanel{
+      sol_name,
+      extract_level
+    });
+  
+  return std::shared_ptr<http_response> (new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response> ProbeSolarPanelResource::render_PUT(const http_request &request) {
+
+  std::string response = "Ok";
+  int response_code = 200;
+  json msg = json::parse(request.get_content());
+
+  if(!msg.contains("command") || !msg.contains("value") || !msg.contains("name")){
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response> (new string_response(response, response_code));
+  }
+
+  std::string command = msg["command"];
+  std::string name = msg["name"];
+  
+  if(command == "name") {
+    if(!sim_model->power_manager().rename_battery(name, msg["value"])){
+      response = "No Solar panel with this name present";
+      response_code = 400;
+    }
+  } else if (command == "extract") {
+    if(!sim_model->power_manager().extract_solar_panel(name, msg["value"])){
+      response = "No Solar panel with this name present";
+      response_code = 400;
+    }
+  } else {
+    response = "Invalid JSON";
+    response_code = 400;
+  }
+
+  return std::shared_ptr<http_response> (new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response>
+ProbeSolarPanelResource::render_DELETE(const http_request &request) {
+
+  std::string response = "Ok";
+  int response_code = 200;
+
+  json msg = json::parse(request.get_content());
+
+  if (!msg.contains("name")) {
+    response = "Invalid JSON";
+    response_code = 400;
+    return std::shared_ptr<http_response>(
+        new string_response(response, response_code));
+  }
+
+  std::string name = msg["name"];
+
+  bool success = sim_model->power_manager().remove_solar_panel(name);
+  if (!success) {
+    response = "No such battery";
+    response_code = 400;
+    return std::shared_ptr<http_response>(
+        new string_response(response, response_code));
+  }
+
+  return std::shared_ptr<http_response>(
+      new string_response(response, response_code));
+}
+
+std::shared_ptr<http_response> ProbeStopResource::render_POST(const http_request &param) {
+  sim_model->stop_simulation();
+  return std::shared_ptr<http_response>(new string_response("Shutting down!"));
+}
+
+std::shared_ptr<http_response> ProbePwrManagerResource::render_PUT(const http_request &request) {
+
+  std::string response = "Ok";
+  int response_code = 200;
+
+  json msg = json::parse(request.get_content());
+
+  if(!msg.contains("value")){
+    response = "Invalid JSON";
+    response_code = 400; 
+    return std::shared_ptr<http_response>(new string_response(response, response_code));
+  }
+
+  sim_model->power_manager().set_pwr_draw(msg["value"]);
+
+  return std::shared_ptr<http_response>(new string_response(response, response_code));
 }
